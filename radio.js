ebe57d7921bdc462a7f4675799fd6127037db6b2
JuliaCarolineS <141650417+JuliaCarolineS@users.noreply.github.com>
2023-09-24 14:00:16 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,45 +1,75 @@
-<script>
-        const audio = document.getElementById('audio');
-        const prevButton = document.getElementById('prevButton');
-        const playPauseButton = document.getElementById('playPauseButton');
-        const nextButton = document.getElementById('nextButton');
+const player = document.querySelector("#player");
+const musicName = document.querySelector("#musicName");
+const playPauseButton = document.querySelector("#playPauseButton");
+const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
+const currentTime = document.querySelector("#currentTime");
+const duration = document.querySelector("#duration");
+const progressBar = document.querySelector(".progress-bar");
+const progress = document.querySelector(".progress");
 
-        const musicas = ['musica1.mp3', 'musica2.mp3', 'musica3.mp3']; // Adicione aqui as URLs das suas músicas
-        let musicaAtual = 0;
-        let isPlaying = false;
+import songs from "./songs.js";
 
-        function tocarMusica(index) {
-            if (index >= 0 && index < musicas.length) {
-                musicaAtual = index;
-                audio.src = musicas[musicaAtual];
-                audio.play();
-                isPlaying = true;
-                playPauseButton.textContent = '⏸️';
-            }
-        }
+const textButtonPlay = "<i class='bx bx-caret-right'></i>";
+const textButtonPause = "<i class='bx bx-pause'></i>";
 
-        prevButton.addEventListener('click', () => {
-            musicaAtual = (musicaAtual - 1 + musicas.length) % musicas.length;
-            tocarMusica(musicaAtual);
-        });
+let index = 0;
 
-        nextButton.addEventListener('click', () => {
-            musicaAtual = (musicaAtual + 1) % musicas.length;
-            tocarMusica(musicaAtual);
-        });
+prevButton.onclick = () => prevNextMusic("prev");
+nextButton.onclick = () => prevNextMusic();
 
-        playPauseButton.addEventListener('click', () => {
-            if (isPlaying) {
-                audio.pause();
-                isPlaying = false;
-                playPauseButton.textContent = '▶️';
-            } else {
-                audio.play();
-                isPlaying = true;
-                playPauseButton.textContent = '⏸️';
-            }
-        });
+playPauseButton.onclick = () => playPause();
 
-        // Tocar a primeira música ao carregar a página
-        tocarMusica(musicaAtual);
-    </script>
+const playPause = () => {
+  if (player.paused) {
+    player.play();
+    playPauseButton.innerHTML = textButtonPause;
+  } else {
+    player.pause();
+    playPauseButton.innerHTML = textButtonPlay;
+  }
+};
+
+player.ontimeupdate = () => updateTime();
+
+const updateTime = () => {
+  const currentMinutes = Math.floor(player.currentTime / 60);
+  const currentSeconds = Math.floor(player.currentTime % 60);
+  currentTime.textContent = currentMinutes + ":" + formatZero(currentSeconds);
+
+  const durationFormatted = isNaN(player.duration) ? 0 : player.duration;
+  const durationMinutes = Math.floor(durationFormatted / 60);
+  const durationSeconds = Math.floor(durationFormatted % 60);
+  duration.textContent = durationMinutes + ":" + formatZero(durationSeconds);
+
+  const progressWidth = durationFormatted
+    ? (player.currentTime / durationFormatted) * 100
+    : 0;
+
+  progress.style.width = progressWidth + "%";
+};
+
+const formatZero = (n) => (n < 10 ? "0" + n : n);
+
+progressBar.onclick = (e) => {
+  const newTime = (e.offsetX / progressBar.offsetWidth) * player.duration;
+  player.currentTime = newTime;
+};
+
+const prevNextMusic = (type = "next") => {
+  if ((type == "next" && index + 1 === songs.length) || type === "init") {
+    index = 0;
+  } else if (type == "prev" && index === 0) {
+    index = songs.length;
+  } else {
+    index = type === "prev" && index ? index - 1 : index + 1;
+  }
+
+  player.src = songs[index].src;
+  musicName.innerHTML = songs[index].name;
+  if (type !== "init") playPause();
+
+  updateTime();
+};
+
+prevNextMusic("init");
