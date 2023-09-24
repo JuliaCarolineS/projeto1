be21af1e9545688e411473d8e8695b1dd44f05f5
JuliaCarolineS <141650417+JuliaCarolineS@users.noreply.github.com>
2023-09-24 14:40:20 -0400

    Update song.js

--- song.js
+++ song.js
@@ -1,26 +1,42 @@
 export default [
   {
-    src: "./src/Dude - Patrick Patrikios.mp3",
-    name: "Dude - Patrick Patrikios",
+    src: "musicas/musica1.mp3",
+    name: "Música 1",
   },
   {
-    src: "./src/Girl Before A Mirror - Verified Picasso.mp3",
-    name: "Girl Before A Mirror - Verified Picasso",
+    src: "musicas/musica2.mp3",
+    name: "Música 2",
   },
   {
-    src: "./src/Like That - Anno Domini Beats.mp3",
-    name: "Like That - Anno Domini Beats",
+   src: "musicas/musica3.mp3",
+    name: "Música 3",
   },
   {
-    src: "./src/Shake - Anno Domini Beats.mp3",
-    name: "Shake - Anno Domini Beats",
+    src: "musicas/musica4.mp3",
+    name: "Música 4",
   },
   {
-    src: "./src/Turn Up Burn Up - Diamond Ortiz.mp3",
-    name: "Turn Up Burn Up - Diamond Ortiz",
+ src: "musicas/musica5.mp3",
+    name: "Música 5",
   },
   {
-    src: "./src/Yo Picasso - Verified Picasso.mp3",
-    name: "Yo Picasso - Verified Picasso",
+  src: "musicas/musica6.mp3",
+    name: "Música 6",
+  },
+  {
+  src: "musicas/musica7.mp3",
+    name: "Música 7",
+  },
+  {
+  src: "musicas/musica8.mp3",
+    name: "Música 8",
+  },
+  {
+  src: "musicas/musica9.mp3",
+    name: "Música 9",
+  },
+  {
+  src: "musicas/musica10.mp3",
+    name: "Música 10",
   },
 ];
